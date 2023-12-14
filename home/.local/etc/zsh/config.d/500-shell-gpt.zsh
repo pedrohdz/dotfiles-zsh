b9824a2fbc502e9a99ee5c20e9574ca19900a5de
Pedro H. <5179251+pedrohdz@users.noreply.github.com>
2023-12-14 09:45:40 +0100

    Shell-GPT aliases

--- home/.local/etc/zsh/config.d/500-shell-gpt.zsh
+++ home/.local/etc/zsh/config.d/500-shell-gpt.zsh
@@ -1,7 +1,32 @@
+# shellcheck shell=bash
+
+if ! which sgpt > /dev/null; then
+  return 0
+fi
+
+sgpt-repl() {
+  if [[ "$1" != --* ]]; then
+    sgpt --repl "${1:-temp}" --shell
+  else
+    echo "ERROR: '$1' is not a chat name!"
+    return 1
+  fi
+}
+
+sgpt-nvim() {
+  if [[ "$1" != --* ]]; then
+    sgpt --chat "${1:-temp}" --editor
+  else
+    echo "ERROR: '$1' is not a chat name!"
+    return 1
+  fi
+}
+
+
 #------------------------------------------------------------------------------
 # *Notes to self (Pedro)*
 #
-# The original version of this file was pulled with:
+# The original version below was pulled with:
 #   ````
 #   wget https://raw.githubusercontent.com/TheR1D/shell_gpt/shell-integrations/simple_zsh.sh
 #   ````
@@ -9,14 +34,13 @@
 # More information can be found in:
 #   - https://github.com/TheR1D/shell_gpt#shell-integration
 #------------------------------------------------------------------------------
-
 # Shell-GPT integration ZSH v0.1
 _sgpt_zsh() {
   if [[ -n "$BUFFER" ]]; then
     _sgpt_prev_cmd=$BUFFER
     BUFFER+=" 󰶡"
     zle -I && zle redisplay
-    BUFFER="$(sgpt --shell <<< $_sgpt_prev_cmd)"
+    BUFFER=" $(sgpt --shell <<< "$_sgpt_prev_cmd")"
     zle end-of-line
   fi
 }
