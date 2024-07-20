64bd6c8e9f0f53ab76be5613c9d37662337ae777
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2024-07-20 08:27:40 +0200

    Turned on CGI-U in WezTerm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -29,6 +29,23 @@ config.window_background_opacity = 0.85
 
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW'
 
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 2,
+  bottom = 2,
+}
+
+-- Setting `enable_csi_u_key_encoding` in hopes of fixing the `CSU U` issue
+-- with NeoVim.  The hope is to allow native support for function keys with
+-- modifiers (i.e. `<S-F1>`).
+--   - https://neovim.io/doc/user/term.html#tui-input
+--
+-- > It  is not recommended to enable this option as it does change the
+-- > behavior of some keys in backwards incompatible ways and there isn't a way
+-- > for applications to detect or request this behavior.gq
+--   - https://wezfurlong.org/wezterm/config/lua/config/enable_csi_u_key_encoding.html
+config.enable_csi_u_key_encoding = true
 
 local key_bindings = require 'pedrohdz.key_bindings'
 key_bindings.apply(config)
