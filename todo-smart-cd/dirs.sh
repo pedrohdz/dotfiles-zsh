d82a8a01ce276613e8525f690c63d95d395cb84c
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2025-03-01 14:38:03 +0100

    FIXME

--- todo-smart-cd/dirs.sh
+++ todo-smart-cd/dirs.sh
@@ -1,7 +1,5 @@
 DIRSTACKSIZE=20
-setopt autopushd pushdsilent pushdtohome # pushdminus 
-
-
+setopt autopushd pushdsilent pushdtohome &> /dev/null  # pushdminus  # FIXME - No STDERR redirect
 
 function __phdz_load_color_variables()
 {
@@ -17,7 +15,7 @@ function __phdz_load_color_variables()
   #   - http://www.commandlinefu.com/commands/view/3584/remove-color-codes-special-characters-with-sed
   #   - http://www.termsys.demon.co.uk/vtansi.htm#colors  (Historical)
 
-  if [[ -z ${__phdz_txt_attr+x} ]]; then
+  if [[ ${#__phdz_txt_attr[@]} -eq 0 ]]; then
     declare -gA __phdz_txt_attr
     __phdz_txt_attr['bold']=$(tput bold)
     __phdz_txt_attr['underline']=$(tput smul)
@@ -26,7 +24,7 @@ function __phdz_load_color_variables()
     __phdz_txt_attr['CLEAR']=$(tput sgr0)
   fi
 
-  if [[ -z ${__phdz_txt_color+x} ]]; then
+  if [[ ${#__phdz_txt_color[@]} -eq 0 ]]; then
     declare -gA __phdz_txt_color
     __phdz_txt_color['black']=$(tput setaf 0)
     __phdz_txt_color['red']=$(tput setaf 1)
@@ -61,9 +59,38 @@ function __phdz_logf_error()
 }
 
 
-#----
+#------------------------------------------------------------------------------
+# dirstack abstraction
+#------------------------------------------------------------------------------
+function __phdz_dirstack_size()
+{
+  if [[ -v dirstack ]]; then
+    echo ${#dirstack[@]}
+  elif [[ -v DIRSTACK ]]; then
+    echo ${#DIRSTACK[@]}
+  else
+    __phdz_logf_error "Invalid shell environment, neither 'dirstack' or 'DIRSTACK' exist."
+    echo 0
+  fi
+}
+
+function __phdz_dirstack_get()
+{
+  local -i _index=$1
+  if [[ -v dirstack ]]; then
+    echo "${dirstack[$_index]}"
+  elif [[ -v DIRSTACK ]]; then
+    echo "${DIRSTACK[$_index]}"
+  else
+    __phdz_logf_error "Invalid shell environment, neither 'dirstack' or 'DIRSTACK' exist."
+    echo ''
+  fi
+}
+
+
+#------------------------------------------------------------------------------
 # Smarter "cd"
-#----
+#------------------------------------------------------------------------------
 function __phdz_dirring_msg()
 {
   local _header=$1
@@ -76,10 +103,11 @@ function __phdz_dirring_msg()
 
 function __phdz_dirring_get_dir()
 {
-  local _user_index=$1
-  local _real_index=$2
+  local -i _user_index=$1
+  local -i _real_index=$2
 
-  local _target=${dirstack[$_real_index]}
+  local _target
+  _target=$(__phdz_dirstack_get $_real_index)
   if [[ -d "$_target" ]]; then
     echo "$_target"
   elif [[ -z "$_target" ]]; then
@@ -93,14 +121,15 @@ function __phdz_dirring_get_dir()
 
 function __phdz_dirring()
 {
-  local -i _dirstack_size=${#dirstack[@]}
-  if [[ ! ${1:=0} =~ ^[+-]{0,1}[0-9]+$ ]]; then
+  local -i _dirstack_size
+  _dirstack_size=$(__phdz_dirstack_size)
+  if [[ ! ${1:-0} =~ ^[+-]{0,1}[0-9]+$ ]]; then
     __phdz_logf_error "Dirstack index of '%s' is invalid.  The index must be between '%d' and '%d'." \
       "$1" $_dirstack_size $((-_dirstack_size))
     return 1
   fi
 
-  local -i _user_index=${1:=0}
+  local -i _user_index=${1:-0}
   if [[ $_user_index -ne 0 ]] && [[ $_dirstack_size -eq 0 ]]
   then
     __phdz_logf_error "Dirstack is empty."
@@ -116,59 +145,53 @@ function __phdz_dirring()
   fi
 
   local _real_index  # Must be string!
-  local _direction
   if [[ $_user_index -eq 0 ]]; then
     builtin dirs -v
     return 0
   elif [[ $_user_index -gt 0 ]]; then
     _real_index="+$_user_index"
-    _direction='backward'
   elif [[ $_user_index -lt 0 ]]; then
-    _real_index="-$(( _user_index + _dirstack_size + 1 ))"
-    _direction='forward'
+    _real_index="-$(( _user_index + _dirstack_size ))"
   fi
 
-  local _target
-  _target=$(__phdz_dirring_get_dir "$_user_index" "$_real_index")
-  [[ -n "$_target" ]] \
+  __phdz_dirring_cd "${_real_index}" \
     || return 1
-
-  __phdz_dirring_msg "Rotating $_direction to" "$_target"
-  # builtin pushd "${_real_index}" > /dev/null \
-  #   || return 1
 }
 
+function __phdz_dirring_cd ()
+{
+  local _new_pwd=${*:-$HOME}
+
+  local _msg
+  if [[ $_new_pwd =~ ^\\+.+ ]]; then
+    _msg='Rotated backward to'
+  elif [[ $_new_pwd =~ ^-.+ ]]; then
+    _msg='Rotated forward to'
+  else
+    _msg='Current working directory'
+  fi
+
+  if ! builtin pushd "$_new_pwd" > /dev/null; then
+    __phdz_logf_error "Failed to change directory: %s" "$_new_pwd"
+    return 1
+  fi
+
+  __phdz_dirring_msg "$_msg" "$PWD"
+  # FIXME - __phdz_set_term_title $PWD
+}
 
 __phdz_load_color_variables
 
 # cd related
-# alias cd='__phdz_dirring_cd'
-# alias ..='__phdz_dirring_cd ..'
-# alias -- -='__phdz_dirring_cd -'
+alias dr='__phdz_dirring_cd'
+alias ..='__phdz_dirring_cd ..'
+alias -- -='__phdz_dirring_cd -'
 alias d='__phdz_dirring'
 # alias r='__phdz_dirring_reverse'
 # alias x='__phdz_dirring_remove'
 
 
 
-
-# function __phdz_dirring_cd ()
-# {
-#   local new_pwd="$*"
-#
-#   if [ -z "$new_pwd" ]; then
-#     new_pwd="$HOME"
-#   fi
-#
-#   builtin pushd "$new_pwd" > /dev/null 2>&1
-#   if [ $? -ne 0 ]; then
-#     printf "${cl_RED}ERROR - Failed to change directory:${cl_NC} $(__phdz_dirring_escape ${new_pwd})\a\n"
-#     return 1
-#   fi
-#
-#   printf "${cl_magenta}Current working directory:${cl_NC} $(__phdz_dirring_escape ${PWD})${cl_NC}\n"
-#   __phdz_set_term_title $PWD
-# }
 
 # function __phdz_dirring_remove ()
 # {
